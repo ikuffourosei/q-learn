e617a6aa3fbe49f9d2493e708327e7a9644cbbb8
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-08-10 23:57:35 +0000

    added some questions and choices to the questions and choices database table

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Questions, Topics, Results, User, Choice
+
+admin.site.register(Questions)
+admin.site.register(Topics)
+admin.site.register(Results)
+admin.site.register(User)
+admin.site.register(Choice)

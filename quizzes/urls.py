1903a0efb08ce7372e5972898ac6899d89e3066e
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-08-15 17:48:45 +0000

    fixed url route namespace errors

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -5,9 +5,9 @@ app_name = "quizzes"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str: topic_name>/', view=views.view_questions, name='questions'),
-    path('<str: topic_name>/submit/', views.submit_quiz, name='submit'),
-    path('<str: topic_name>/results/', views.result, name='results'),
+    path('<str:topic_name>/', view=views.view_questions, name='questions'),
+    path('<str:topic_name>/submit/', views.submit_quiz, name='submit'),
+    path('<str:topic_name>/results/', views.result, name='results'),
     path('login/', views.login_check, name='login'),
     path('logout/', views.logout_check, name='logout'),
     path('register/', views.register, name='register'),
